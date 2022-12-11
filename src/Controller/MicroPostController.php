ddd6dbea71f7bf992d415878d612f450a088b460
PUMCZACZ <pawel.cebernik02@gmail.com>
2022-12-11 14:23:27 +0100

    Added data from form to db

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class MicroPostController extends AbstractController
 {
@@ -29,14 +30,24 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2 )]
-    public function add(): Response
+    public function add(Request $request, MicroPostRepository $posts): Response
     {
         $microPost = new MicroPost();
         $form = $this->createFormBuilder($microPost)
-        ->add('title')
-        ->add('text')
-        ->add('submit', SubmitType::class, ['label' => 'Save'])
-        ->getForm();
+            ->add('title')
+            ->add('text')
+            ->add('submit', SubmitType::class, ['label' => 'Save'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->save($post, true);
+        }
+
 
         return $this->renderForm('micro_post/add.html.twig', [
             
