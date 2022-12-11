3cea6c71bc5ee7df3927f812a5e128cf167409e8
PUMCZACZ <pawel.cebernik02@gmail.com>
2022-12-11 13:01:38 +0100

    Add Forms into Project

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,12 +2,13 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
-use DateTime;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
 {
@@ -25,5 +26,23 @@ class MicroPostController extends AbstractController
         return $this->render('micro_post/show.html.twig', [
             'post' => $post,
         ]);
+    }
+
+    #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2 )]
+    public function add(): Response
+    {
+        $microPost = new MicroPost();
+        $form = $this->createFormBuilder($microPost)
+        ->add('title')
+        ->add('text')
+        ->add('submit', SubmitType::class, ['label' => 'Save'])
+        ->getForm();
+
+        return $this->renderForm('micro_post/add.html.twig', [
+            
+                'form' => $form
+            ]
+        );
+            
     }
 }
