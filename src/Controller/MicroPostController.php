6d58a31fed537976ce3b3633dfbe44524491a7b7
PUMCZACZ <pawel.cebernik02@gmail.com>
2022-12-13 18:24:27 +0100

    Added tailwindcss into project and styled post section

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use DateTime;
 use App\Entity\MicroPost;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -32,11 +33,7 @@ class MicroPostController extends AbstractController
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2 )]
     public function add(Request $request, MicroPostRepository $posts): Response
     {
-        $microPost = new MicroPost();
-        $form = $this->createFormBuilder($microPost)
-            ->add('title')
-            ->add('text')
-            ->getForm();
+        $form = $this->createForm(MicroPostType::class, new MicroPost());
 
         $form->handleRequest($request);
 
@@ -47,6 +44,33 @@ class MicroPostController extends AbstractController
             $posts->save($post, true);
 
             $this->addFlash('succes', 'Your micro post have been added');
+
+            return $this->redirectToRoute('app_micro_post');
+        }
+
+
+        return $this->renderForm('micro_post/add.html.twig', [
+            
+                'form' => $form
+            ]
+        );
+            
+    }
+
+    #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
+    public function edit(MicroPost $post,Request $request, MicroPostRepository $posts): Response
+    {
+        $form = $this->createForm(MicroPostType::class, $post);
+
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $post = $form->getData();
+            $posts->save($post, true);
+
+            $this->addFlash('succes', 'Your micro post have been updated');
 
             return $this->redirectToRoute('app_micro_post');
         }
